f1085cd2e5287101edc6106c8f8d107b014d6e78
samar sayed <sayedsamar58@gmail.com>
2025-11-07 00:56:08 +0530

    signin endpoint added

---
+++ src/app/api/signin/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "../../../../generated/prisma/client";
+
+
+const prisma = new PrismaClient()
+const secret = process.env.JWT_SECRET || "secret"
+
+export async function POST(req:NextRequest,res:NextResponse){
+
+const hardcodedWalletAddress = "6k9VSg4bVvVAj5xVis9eG74vED9iWYXjhSUCnA8gzChR";
+
+const existingUser = await prisma.user.findFirst({
+    where: {
+        address:hardcodedWalletAddress
+    }
+})
+
+if(existingUser){
+const token = jwt.sign({
+      userId:existingUser.id
+},secret)
+
+return NextResponse.json({ token })
+}
+
+else{
+const newUser = await prisma.user.create({
+    data: {
+        address:hardcodedWalletAddress
+    }
+})
+
+const token = jwt.sign({
+    userId:newUser.id
+},secret)
+
+return NextResponse.json({ token })
+}
+}

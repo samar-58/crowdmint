f3163c8f85a8b1ea509f390402bfd8e4126a5f02
samar sayed <sayedsamar58@gmail.com>
2025-11-08 17:42:22 +0530

    Get endpoint created for tasks

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -2,6 +2,7 @@ import { createTaskSchema } from "@/types/createTask";
 import { prisma } from "@/utils/constants";
 import { NextRequest, NextResponse } from "next/server";
 import { TaskType } from "../../../../generated/prisma/client";
+import z from "zod";
 
 
 export async function POST(req:NextRequest){
@@ -35,4 +36,52 @@ export async function POST(req:NextRequest){
    return task;
     }) 
     return NextResponse.json({ message: "Task created successfully" ,taskId:response?.id}, { status: 200 });
+}
+
+
+export async function GET(req:NextRequest){
+ const taskId = z.string().safeParse(req.nextUrl.searchParams.get("taskId"));
+
+const userId = req.headers.get("x-user-id");
+
+ const task = await prisma.task.findUnique({
+    where:{
+        id:taskId.data,
+        userId:userId as string,
+    },
+    include:{
+        options:true,
+    }
+ })
+ if(!task){
+    return NextResponse.json({ error: "Task not found" }, { status: 404 });
+ }
+
+const submissions = await prisma.submission.findMany({
+    where:{
+        taskId:task.id,
+    },
+    include:{
+  option:true,
+    }
+ })
+ const result : Record<string, {
+    count:number,
+    option:{
+        imageUrl:string,
+    }
+ }> = {};
+task.options.forEach(option=>{
+    result[option.id] = {
+        count:0,
+        option:{
+            imageUrl:option.imageUrl as string,
+        }
+    }
+})
+ submissions.forEach(submission => {
+    result[submission.optionId].count++;
+ })
+
+ return NextResponse.json({ result }, { status: 200 });
 }

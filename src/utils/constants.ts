d69ac0cd8aaff5045b22727f502103fea77d6f60
samar sayed <sayedsamar58@gmail.com>
2025-11-22 22:40:13 +0530

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL
+        }
+    }
+})
 
 export const LAMPORTS_PER_SOL = 10 ** 9;
 
-export const PARENT_WALLET_ADDRESS = process.env.PARENT_WALLET_ADDRESS as string?? "FdYARKsLYMSWExkxLedf5VdeGbLWp8Y4iTTKMK2G8mDh";
+export const PARENT_WALLET_ADDRESS = process.env.PARENT_WALLET_ADDRESS as string ?? "FdYARKsLYMSWExkxLedf5VdeGbLWp8Y4iTTKMK2G8mDh";
 
 export const PARENT_WALLET_SECRET_KEY = process.env.PARENT_WALLET_SECRET_KEY as string ?? ""
